fee0339d0eb1ae23d8236744d4f2aa9f20aa9f59
Lucas <luc.coulson6@gmail.com>
2025-08-28 18:44:12 -0400

    created the face detector using haar cascades

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import cv2 as cv
 import mediapipe as mp 
 import numpy as np 
 
-face_casc = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cam = cv.VideoCapture(1)
 
 while True:
     ret, img = cam.read()
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img_gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        roi_gray = img_gray[x:x+w, y:y+h]
+        roi_color = img[x:x+w, y:y+h]
+
+    
+
 
 
     cv.imshow('img', img)
